b2b4eeed0fbba8de9d1da62259fcb053ee91644f
ashrafulanam05 <66620937+ashrafulanam05@users.noreply.github.com>
2020-06-25 18:06:56 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Peer-graded Assignment: Bash, Make, Git, and GitHub
+ **Make** was run at:
+Thu Jun 25 17:58:55 CEST 2020
+The total number of lines of the script is:
+20
---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+function guess(){
+    real_num=$(ls | wc -l)
+    while true;
+    do
+        echo "Enter a guessed number: "
+        read  guess_num
+        if [ $guess_num -lt $real_num ]
+        then
+            echo "The number you have guessed a too low number than original number of directory.Please try again"
+        elif [ $guess_num -gt $real_num ]
+        then
+            echo "The number you have guessed a too high number than original number of directory.Please try againr"
+        else
+            echo " Congratulation you have guessed right number "
+        break;
+        fi
+    done
+}
+echo "You should guess the number of files in the current directory!"
+guess
